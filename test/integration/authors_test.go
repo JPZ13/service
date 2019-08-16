b2a871888f9171b21ec1536bff20801ae2773637
J.P. Zivalich <j.p.zivalich@gmail.com>
2019-08-16 10:56:43 -0400

    Refactor create author test to be reusable

--- test/integration/authors_test.go
+++ test/integration/authors_test.go
@@ -13,13 +13,18 @@ import (
 func TestCreateAuthor(t *testing.T) {
 	t.Parallel()
 
+	createAuthor(t, "Mark", "Danielewski")
+}
+
+func createAuthor(t *testing.T, firstname, lastname string) {
 	ctx := context.Background()
 	author, err := testClient.CreateAuthor(ctx, &model.CreateAuthorRequest{
-		FirstName: "Mark",
-		LastName:  "Danielewski",
+		FirstName: firstname,
+		LastName:  lastname,
 	})
+
 	require.NoError(t, err)
-	require.Equal(t, "Mark", author.FirstName)
-	require.Equal(t, "Danielewski", author.LastName)
+	require.Equal(t, firstname, author.FirstName)
+	require.Equal(t, lastname, author.LastName)
 	require.NotNil(t, author.UUID)
 }
