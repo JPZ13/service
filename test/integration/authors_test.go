94614ea332311a201c83c0a83b7b74ca71321589
J.P. Zivalich <j.p.zivalich@gmail.com>
2019-08-16 10:53:35 -0400

    Add create author test and fix db foreign key

--- test/integration/authors_test.go
+++ test/integration/authors_test.go
@@ -3,14 +3,23 @@
 package integration
 
 import (
-	"errors"
+	"context"
 	"testing"
 
+	"github.com/JPZ13/service/model"
 	"github.com/stretchr/testify/require"
 )
 
-// Placeholder for future integration tests
-func TestPlaceholder(t *testing.T) {
-	err := errors.New("placeholder error")
-	require.Error(t, err)
+func TestCreateAuthor(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	author, err := testClient.CreateAuthor(ctx, &model.CreateAuthorRequest{
+		FirstName: "Mark",
+		LastName:  "Danielewski",
+	})
+	require.NoError(t, err)
+	require.Equal(t, "Mark", author.FirstName)
+	require.Equal(t, "Danielewski", author.LastName)
+	require.NotNil(t, author.UUID)
 }
